471e72e78ef14d693ae5f1098e24b8b585ad3f74
kanno <812137533@qq.com>
2022-02-21 11:50:58 +0800

    feat: note add stamp and title

---
+++ src/components/posts-title/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="posts-title">
+    <h1>{{ title }}</h1>
+    <div class="date-box">
+      <p>
+        <span>{{ new Date(date).toLocaleString('zh-cn') }}</span>
+        <span class="septor"> > </span>
+        <span>{{ beforeTransfer(date) }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { timeConver } from '../../utils'
+export default defineComponent({
+  props: {
+    title: String,
+    date: String
+  },
+  setup() {
+    const beforeTransfer = (time: string) => {
+      return timeConver(time)
+    }
+    return {
+      beforeTransfer
+    }
+  }
+})
+</script>
+
+<style lang="css" scoped>
+.posts-title > h1 {
+  font-size: 2rem;
+}
+
+.posts-title .date-box p {
+  font-size: 0.9rem;
+}
+.posts-title .date-box p span {
+  padding: 0 0.2rem;
+  height: 100%;
+}
+
+.posts-title .septor {
+  font-weight: bold;
+  color: var(--success-light);
+}
+</style>
